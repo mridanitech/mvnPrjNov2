da5abfad933bfce4171c2210bf5c486e943925a4
user1 <user1@localhost.com>
2019-11-02 13:34:25 +0530

    Initial versoin

---
+++ src/main/java/com/mridanitech/training/mvnPrjNov3/App.java
@@ -0,0 +1,13 @@
+package com.mridanitech.training.mvnPrjNov2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
